95a3d17421cc13a6c8171d7e2d778d512fb1f347
Jaedsonn <jaedsonbarbosamacedo@gmail.com>
2024-08-11 17:51:41 -0300

    feat: atividade 6( comparação de números) feita!

--- src/exercicios/lista_1/NumCompare.java
+++ src/exercicios/lista_1/NumCompare.java
@@ -5,7 +5,19 @@ import java.util.Scanner;
 public class NumCompare {
     private static final Scanner io = new Scanner(System.in);
 
-    public static void main(String[] args){
-        System.out.println("");
+    public static void main(String[] args) {
+        System.out.println("Vamos comparar número!");
+
+        System.out.print("Primeiro número: ");
+        double firstDigit = io.nextDouble();
+
+        System.out.print("Segundo número: ");
+        double secondDigit = io.nextDouble();
+
+        if (firstDigit > secondDigit) {
+            System.out.println(firstDigit + " é maior que " + secondDigit);
+        } else {
+            System.out.println((secondDigit + " é maior que " + firstDigit));
+        }
     }
 }
