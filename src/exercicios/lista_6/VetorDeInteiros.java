5b996efff9e7988a4f2077041887540dcf7e758e
Jaedsonn <jaedsonbarbosamacedo@gmail.com>
2024-08-22 11:36:01 -0300

    fix: resolve de minValue problem if it's zero

--- src/exercicios/lista_6/VetorDeInteiros.java
+++ src/exercicios/lista_6/VetorDeInteiros.java
@@ -4,7 +4,7 @@ public class VetorDeInteiros {
 
     public static void main(String[] args) {
         int[] inteiros = new int[10];
-        inteiros[0] = 5;
+        inteiros[0] = 32;
         inteiros[1] = 53;
         inteiros[2] = 12;
         inteiros[3] = 43;
@@ -12,10 +12,10 @@ public class VetorDeInteiros {
         inteiros[5] = 12;
         inteiros[6] = 43;
         inteiros[7] = 54;
-        inteiros[8] = 2;
+        inteiros[8] = 43;
         inteiros[9] = 10;
 
-        int menorValor = 0;
+        int menorValor = inteiros[0];
         int maiorValor = 0;
 
         for (int i = 0; i < inteiros.length; i++) {
@@ -23,9 +23,7 @@ public class VetorDeInteiros {
             if (inteiros[i] > maiorValor) {
                 maiorValor = inteiros[i];
             }
-            if (menorValor == 0) {
-                menorValor = inteiros[0];
-            }
+
             if (menorValor > inteiros[i]) {
                 menorValor = inteiros[i];
             }
