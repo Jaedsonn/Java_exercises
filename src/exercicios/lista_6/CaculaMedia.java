9b01544d07417eb81f52eb415175f8d6618b8fde
Jaedsonn <jaedsonbarbosamacedo@gmail.com>
2024-08-22 11:39:09 -0300

    refactor: move the logic for calcularMedia method and change the param for receive the array

--- src/exercicios/lista_6/CaculaMedia.java
+++ src/exercicios/lista_6/CaculaMedia.java
@@ -6,20 +6,21 @@ public class CaculaMedia {
         int[] numeros = new int[5];
         numeros[0] = 5;
         numeros[1] = 10;
-        numeros[2] = 5;
+        numeros[2] = 0;
         numeros[3] = 15;
         numeros[4] = 20;
 
+
+
+        System.out.println("A média é " + calcularMedia(numeros));
+    }
+
+    public static double calcularMedia(int[] numeros) {
         int sum = 0;
 
         for (int num : numeros) {
             sum += num;
         }
-
-        System.out.println("A média é " + calcularMedia(sum, numeros.length));
-    }
-
-    public static double calcularMedia(int sum, int len) {
-        return sum / len;
+        return sum / numeros.length;
     }
 }
